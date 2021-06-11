8412ac04389f764f122f9bc0773f7bc281e8f07b
MINSK\Dzmitry_Paulavets <Dzmitry_Paulavets@epam.com>
2021-06-11 19:23:57 +0300

    NewsApi

---
+++ FeedAPI/FeedAPI/FeedAPI/Controllers/NewsAPIController.cs
@@ -0,0 +1,30 @@
+﻿using FeedAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeedAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsAPIController : ControllerBase
+    {
+        private readonly INewsApi newsApi;
+
+        public NewsAPIController(INewsApi _newsApi)
+        {
+            newsApi = _newsApi;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            JsonResult json = newsApi.Get();
+
+            return new JsonResult(json);
+        }
+    }
+}
--- FeedAPI/FeedAPI/FeedAPI/Services/INewsApi.cs
+++ FeedAPI/FeedAPI/FeedAPI/Services/INewsApi.cs
@@ -1,20 +1,35 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace FeedAPI.Services
 {
     public interface INewsApi
     {
-        string Test();
+        public JsonResult Get();
     }
 
     public class NewsApi : INewsApi
     {
-        public string Test()
+
+        [HttpGet]
+        public JsonResult Get()
         {
-            return new string("NewsApi Test");
+
+            var url = "https://newsapi.org/v2/everything?" +
+                      "q=Apple&" +
+                      "from=2021-06-11&" +
+                      "sortBy=popularity&" +
+                      "apiKey=d11afab6486343cfa40068ffd60f9e68";
+
+            var json = new WebClient().DownloadString(url);
+
+            Console.WriteLine(json);
+
+            return new JsonResult(json);
         }
     }
 }
