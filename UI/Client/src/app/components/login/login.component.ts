5cf9d0b453058569f6a72eeb7cee9af2aa814710
Vladimirkasperovich <vova.kasper.boy@gmail.com>
2023-10-31 16:28:05 +0300

    add Englesh and Russian language

--- UI/Client/src/app/components/login/login.component.ts
+++ UI/Client/src/app/components/login/login.component.ts
@@ -14,7 +14,7 @@ export class LoginComponent implements OnInit {
 
   myForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.pattern('^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$')]),
+    password: new FormControl('', [Validators.required]),
     phrase: new FormControl('', [Validators.required, Validators.pattern('')]),
     rememberMe: new FormControl('', [Validators.required]),
   });
@@ -30,6 +30,7 @@ export class LoginComponent implements OnInit {
   isShowOrHideValueInputPhrase: boolean = false;
   text:string = 'text';
   password:string = 'password';
+  localePath:string ='Pages/LoginPage/'
 
   constructor(private authService: AuthService, private sessionService: SessionService) {
   }
