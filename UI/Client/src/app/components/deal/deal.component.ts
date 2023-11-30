1a1d342e2691ebe6b2de2c144deb89462c006219
Vladimirkasperovich <vova.kasper.boy@gmail.com>
2023-11-30 16:17:23 +0300

    ChangeDealComponent

--- UI/Client/src/app/components/deal/deal.component.ts
+++ UI/Client/src/app/components/deal/deal.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
-import { ActivatedRoute } from '@angular/router';
-import { IDeal } from 'src/app/Models/IDeal';
-import { DealService } from 'src/app/services/deal.service';
+import {Component, OnInit} from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
+import {ActivatedRoute, Router} from '@angular/router';
+import {IDeal} from 'src/app/Models/IDeal';
+import {DealService} from 'src/app/services/deal.service';
+import {SessionService} from "src/app/services/session.service";
 
 @Component({
   selector: 'app-deal',
@@ -17,21 +18,26 @@ export class DealComponent implements OnInit {
   constructor(
     private domSanitizer: DomSanitizer,
     private route: ActivatedRoute,
-    private readonly dealService: DealService
-    ) { }
+    private readonly dealService: DealService,
+    private router: Router,
+    public userSession: SessionService
+  ) {
+  }
 
   ngOnInit(): void {
     var id = this.route.snapshot.params.id as unknown as number;
 
     this.dealService.getDeal(id).subscribe(deal => {
-      this.deal = deal;
 
+      this.deal = deal;
       this.renderImages(deal);
     });
   }
-  
+  redirectToChangeDeal(){
+    this.router.navigate([`changeDeal/${this.deal.id}`])
+  }
   renderImages(deal: IDeal) {
-    deal.assets?.forEach((asset: any) => {      
+    deal.assets?.forEach((asset: any) => {
       if (asset == undefined || asset.imageData == "") return;
       this.base64Image = this.domSanitizer.bypassSecurityTrustUrl("data:image/jpeg;base64, " + asset.imageData);
     });
